20ab7e331b1d6300ddc259fc3e99197aff760f8e
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-10-26 19:29:50 -0300

    refactor Move tests to specific folder Since by default Next.js tries to treat all files in the /pages directory as pages for routing, the tests will be moved to another folder

--- __tests__/discover/discover.test.js
+++ __tests__/discover/discover.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
-import Discover from "./index";
+import Discover from "../../pages/discover";
 
 describe("Discover page", () => {
   it("renders the text Discover", () => {
