2cd82a6bdbb322fadfa2012c278eb0193fd530c0
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-10-26 16:19:36 -0300

    feature/CROW-18 Add test coverage to CI

---
+++ pages/discover/index.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import Discover from "./index";
+
+describe("Discover page", () => {
+  it("renders the text Discover", () => {
+    render(<Discover />);
+
+    expect(screen.getByText("Discover")).toHaveTextContent("Discover");
+  });
+});
