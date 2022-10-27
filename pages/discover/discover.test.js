b7832436ba0f7589633a9159e3f71ecb82f95ea2
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-10-27 15:11:54 -0300

    Move tests to the pages folder

--- pages/discover/discover.test.js
+++ pages/discover/discover.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
-import Discover from "../../pages/discover";
+import Discover from "./index.page";
 
 describe("Discover page", () => {
   it("renders the text Discover", () => {
