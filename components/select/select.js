5a1991848e1c11eecd1c4c6b09326fec644c2e30
ariaseugeniab <ariaseugeniab@gmail.com>
2022-11-16 11:15:32 -0300

    CROW-50 Add create page

--- components/select/select.js
+++ components/select/select.js
@@ -3,6 +3,9 @@ import React from "react";
 const Select = ({ options, name, id }) => {
   return (
     <select name={name} id={id}>
+      <option key="select" value="select" selected>
+        Select...
+      </option>
       {options.map((opt) => (
         <option key={opt.value} value={opt.value}>
           {opt.name}
--- pages/create/index.page.js
+++ pages/create/index.page.js
@@ -1,5 +1,79 @@
+import Button from "../../components/button/button";
+import Input from "../../components/input/input";
+import Select from "../../components/select/select";
+
 const Create = () => {
-  return <div>Create a new Project</div>;
+  return (
+    <div>
+      <h1>Create New Project</h1>
+      <form
+        style={{ display: "flex", flexDirection: "column", gap: "20px" }}
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <label>
+          Project Name
+          <Input type="text" name="project-name" style={{ display: "block" }} />
+        </label>
+        <label>
+          Subtitle
+          <Input type="text" name="subtitle" />
+        </label>
+        <label>
+          Category
+          <Select
+            type="text"
+            name="category"
+            id="1"
+            options={[
+              { value: "technology", name: "Technology" },
+              { value: "nfts", name: "NFTs" },
+              { value: "games", name: "Games" },
+              { value: "education", name: "Education" },
+              { value: "music", name: "Music" },
+              { value: "community", name: "Community" },
+              { value: "non-profit", name: "Non profit" },
+              { value: "other", name: "Other" },
+            ]}
+          />
+        </label>
+        <label>
+          Description
+          <Input type="text-area" name="description" />
+        </label>
+        <label>
+          Currency
+          <Select
+            type="text"
+            name="currency"
+            id="1"
+            options={[
+              { value: "USD", name: "USD" },
+              { value: "BTC", name: "BTC" },
+              { value: "ETH", name: "ETH" },
+              { value: "NGN", name: "NGN" },
+              { value: "EUR", name: "EUR" },
+            ]}
+          />
+        </label>
+        <label>
+          Fund Goal
+          <Input type="number" name="fund-goal" />
+        </label>
+        <p>Dates</p>
+        <label>
+          Start Date
+          <Input type="date" name="start-date" />
+        </label>
+        <label>
+          End Date
+          <Input type="date" name="end-date" />
+        </label>
+        <Button type="submit">Create</Button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
