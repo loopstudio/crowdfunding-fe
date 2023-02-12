5b6943ecefb5a677836faffbe1f137fb27f01dc6
Marcos Martínez <mmartinezciompi@gmail.com>
2023-02-12 15:14:29 -0300

    Fix use correct url for backend

--- pages/login/index.page.js
+++ pages/login/index.page.js
@@ -9,65 +9,65 @@ import { ConnectWallet } from "../../components";
 import { Container, StyledButton } from "./login.styles";
 
 const Login = () => {
-  const router = useRouter();
-  const { address, isConnected } = useAccount();
-  const [nonce, setNonce] = useState(null);
+    const router = useRouter();
+    const { address, isConnected } = useAccount();
+    const [nonce, setNonce] = useState(null);
 
-  const login = async (address, data) => {
-    try {
-      const res = await axios.post(
-        `${process.env.NEXT_PUBLIC_CROWDFUNDING_API}/api/v1/auth/login`,
-        {
-          publicAddress: address,
-          signature: data,
+    const login = async (address, data) => {
+        try {
+            const res = await axios.post(
+                `${process.env.NEXT_PUBLIC_CROWDFUNDING_API}/auth/login`,
+                {
+                    publicAddress: address,
+                    signature: data,
+                }
+            );
+
+            const accessToken = res.data.data.accessToken;
+            sessionStorage.setItem(ACCESS_TOKEN, accessToken);
+
+            router.push("/");
+        } catch (error) {
+            console.error(`Error logging in: ${error}`);
         }
-      );
+    };
 
-      const accessToken = res.data.data.accessToken;
-      sessionStorage.setItem(ACCESS_TOKEN, accessToken);
+    const { signMessage } = useSignMessage({
+        message: `Signing login nonce: ${nonce}`,
+        onSuccess(data) {
+            login(address, data);
+        },
+    });
 
-      router.push("/");
-    } catch (error) {
-      console.error(`Error logging in: ${error}`);
-    }
-  };
+    const fetchNonce = async () => {
+        try {
+            const res = await axios.get(
+                `${process.env.NEXT_PUBLIC_CROWDFUNDING_API}/api/v1/auth/${address}/nonce`
+            );
+            setNonce(res.data.data);
+        } catch (error) {
+            console.log(`Error querying nonce: ${error}`);
+        }
+    };
 
-  const { signMessage } = useSignMessage({
-    message: `Signing login nonce: ${nonce}`,
-    onSuccess(data) {
-      login(address, data);
-    },
-  });
+    useEffect(() => {
+        fetchNonce();
+    }, [address]);
 
-  const fetchNonce = async () => {
-    try {
-      const res = await axios.get(
-        `${process.env.NEXT_PUBLIC_CROWDFUNDING_API}/api/v1/auth/${address}/nonce`
-      );
-      setNonce(res.data.data);
-    } catch (error) {
-      console.log(`Error querying nonce: ${error}`);
-    }
-  };
+    useEffect(() => {
+        const accessToken = sessionStorage.getItem(ACCESS_TOKEN);
 
-  useEffect(() => {
-    fetchNonce();
-  }, [address]);
+        if (accessToken) router.push("/");
+    }, []);
 
-  useEffect(() => {
-    const accessToken = sessionStorage.getItem(ACCESS_TOKEN);
-
-    if (accessToken) router.push("/");
-  }, []);
-
-  if (!isConnected) return <ConnectWallet />;
-  return (
-    <Container>
-      <StyledButton onClick={() => signMessage()} disabled={!nonce}>
-        Login
-      </StyledButton>
-    </Container>
-  );
+    if (!isConnected) return <ConnectWallet />;
+    return (
+        <Container>
+            <StyledButton onClick={() => signMessage()} disabled={!nonce}>
+                Login
+            </StyledButton>
+        </Container>
+    );
 };
 
 export default Login;
