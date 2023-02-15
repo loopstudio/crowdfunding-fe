2d144dc507b788373082b4a9523427816faaa824
Lucas-Maurojorge <lmaurojorge@loopstudio.dev>
2023-02-15 16:10:20 -0300

    validation added to date picker

--- hooks/useCreateForm.js
+++ hooks/useCreateForm.js
@@ -1,7 +1,7 @@
-import { useEffect } from "react";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
-import { object, string, date, number, ref } from "yup";
+import { useEffect } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import { object, string, date, number, ref } from 'yup';
 import {
   TITLE,
   SUBTITLE,
@@ -10,7 +10,7 @@ import {
   FUND_GOAL,
   START_DATE,
   END_DATE,
-} from "constants";
+} from 'constants';
 
 const validationSchema = object().shape({
   [TITLE]: string().required(),
@@ -20,10 +20,22 @@ const validationSchema = object().shape({
   [FUND_GOAL]: number().required(),
   [START_DATE]: date()
     .required()
-    .min(new Date(), "Date should be greater than now"),
+    .min(new Date(), 'Date should be greater than now'),
   [END_DATE]: date()
     .required()
-    .min(ref("startDate"), "End date should be after start date"),
+    .min(ref('startDate'), 'End date should be after start date')
+    .test(
+      'maxDate',
+      'End date should not be more than 20 days after start date',
+      function (value) {
+        const startDate = this.resolve(ref('startDate'));
+        const endDate = value;
+        const differenceInDays = Math.floor(
+          (endDate - startDate) / (1000 * 60 * 60 * 24)
+        );
+        return differenceInDays <= 20;
+      }
+    ),
 });
 
 export const useCreateForm = () => {
@@ -37,7 +49,7 @@ export const useCreateForm = () => {
   } = useForm({
     resolver: yupResolver(validationSchema),
     defaultValues: {},
-    mode: "onChange",
+    mode: 'onChange',
   });
 
   const handleTokenSelect = (event) => {
