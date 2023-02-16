9b51b2d9d72fdc3ed415e5a5441e1b02cea8a981
FacuEM <facundomartinezesc@gmail.com>
2023-02-16 12:48:02 -0300

    remove comment

--- pages/pledge/pledge.styles.js
+++ pages/pledge/pledge.styles.js
@@ -19,7 +19,6 @@ export const ImageContainer = styled.div`
 `;
 
 export const ImageBanner = styled.img`
-  /* background: url(${(props) => props.src}); */
   width: 100%;
   height: 100%;
   display: flex;
