ecd386eaf4665375ba6daddd7ff51fa2e604b5b1
FacuEM <facundomartinezesc@gmail.com>
2023-02-28 16:00:51 -0300

    Add search functionality

--- components/sideBar/sideBar.styles.js
+++ components/sideBar/sideBar.styles.js
@@ -9,20 +9,17 @@ export const SideBarContainer = styled.div`
   justify-content: flex-start;
   padding: 40px 70px 40px 40px;
   left: 0px;
-  width: 250px;
-  height: 100vh;
+  min-height: 100vh;
   background-color: #ffffff0f;
   flex-direction: column;
   gap: 18px;
   white-space: nowrap;
 `;
 
-export const Title = styled.h2`
+export const Title = styled.span`
   font-size: 24px;
-  margin-top: 40px;
 `;
 
 export const Subtitle = styled.a`
   font-size: 14px;
-  margin-top: 15px;
 `;
