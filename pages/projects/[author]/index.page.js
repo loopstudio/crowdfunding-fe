18cfbe8135f6a085389b52161a75d784eb2e1db0
FacuEM <facundomartinezesc@gmail.com>
2023-03-14 09:37:20 -0300

    remove unnused pages

--- pages/projects/[author]/index.page.js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-
-const AuthorProjects = () => {
-  const router = useRouter();
-  const { author } = router.query;
-  return <div>Projects by {author}</div>;
-};
-
-export default AuthorProjects;
