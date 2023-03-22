5cf4c5d10609954ab38b9b448fa40cb7edab0bb8
FacuEM <facundomartinezesc@gmail.com>
2023-03-22 11:34:31 -0300

    Move modal styles to const

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -2,44 +2,28 @@ import ReactModal from "react-modal";
 import { useRouter } from "next/router";
 
 import { Button } from "components";
+import { GoBackModalStyles } from "utils/modal";
 
 import { Question, Description, ButtonsContainer } from "./modal.styles";
 
 export const Modal = ({ isModalOpen, handleModal }) => {
   const router = useRouter();
 
+  const handleClose = () => {
+    handleModal(false);
+  };
+
+  const handleConfirm = () => {
+    router.push("/");
+  };
+
   return (
     <ReactModal
       isOpen={isModalOpen}
       shouldCloseOnEsc
       shouldCloseOnOverlayClick
-      onRequestClose={() => {
-        handleModal(false);
-      }}
-      style={{
-        overlay: {
-          backgroundColor: "rgba(0, 0, 0, 0.5)",
-        },
-        content: {
-          background: "#1B1B1B",
-          border: "1px solid rgba(255,255,255,0.25)",
-          padding: "24px",
-          display: "flex",
-          flexDirection: "column",
-          gap: "24px",
-          alignItems: "flex-start",
-          width: "540px",
-          height: "217px",
-          justifyContent: "space-between",
-          boxShadow: "0px 4px 24px 0px #FFFFFF33",
-          borderRadius: "0px",
-          position: "absolute",
-          top: "200px",
-          left: "400px",
-          right: "40px",
-          bottom: "40px",
-        },
-      }}
+      onRequestClose={handleClose}
+      style={GoBackModalStyles}
     >
       <Question>Are you sure?</Question>
 
@@ -49,14 +33,8 @@ export const Modal = ({ isModalOpen, handleModal }) => {
       </Description>
 
       <ButtonsContainer>
-        <Button
-          onClick={() => {
-            handleModal(false);
-          }}
-        >
-          No
-        </Button>
-        <Button onClick={() => router.push("/")}>Yes</Button>
+        <Button onClick={handleClose}>No</Button>
+        <Button onClick={handleConfirm}>Yes</Button>
       </ButtonsContainer>
     </ReactModal>
   );
---
+++ utils/modal.js
@@ -0,0 +1,24 @@
+export const GoBackModalStyles = {
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  content: {
+    background: "#1B1B1B",
+    border: "1px solid rgba(255,255,255,0.25)",
+    padding: "24px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "24px",
+    alignItems: "flex-start",
+    width: "540px",
+    height: "217px",
+    justifyContent: "space-between",
+    boxShadow: "0px 4px 24px 0px #FFFFFF33",
+    borderRadius: "0px",
+    position: "absolute",
+    top: "200px",
+    left: "400px",
+    right: "40px",
+    bottom: "40px",
+  },
+};
